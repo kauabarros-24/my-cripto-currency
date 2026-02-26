18829d944bf4ef23b0f131050914cf2fee0f3e6c
user.name: kauabarros-25 <user.mail: martinsbarroskaua@gmail.com>
2026-02-26 20:03:04 -0300

    Fiz merda

---
+++ src/blockchain.py
@@ -0,0 +1,41 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+    
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transcations': self.current_transaction,
+            'proof': proof,
+        }
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+        
+    
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transaction({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
+    
+    
+    def last_block(self):
+        return self.chain[-1w]
+ 
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
