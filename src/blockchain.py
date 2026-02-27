3ba6b12e3c87de16d32797579f70622ad137dcaa
user.name: kauabarros-25 <user.mail: martinsbarroskaua@gmail.com>
2026-02-27 08:47:59 -0300

    Implementing basic Proof of Work

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
@@ -38,3 +39,20 @@ class Blockchain(object):
     @property
     def last_block(self):
         return self.chain[-1]
+    
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+            
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+    
+    
+    
+            
